91fd133e7b1bffaf6c98b9397fdc87188b73f9c9
OhdeD <dagmara.ohde@gmail.com>
2019-07-30 23:31:37 +0000

    background

--- src/main/java/com/kodilla/checkers/CheckersApplication.java
+++ src/main/java/com/kodilla/checkers/CheckersApplication.java
@@ -1,24 +1,27 @@
 package com.kodilla.checkers;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 
 public class CheckersApplication extends Application {
 
-    private Image imageback = new Image("file:resources/background.jpg");
+    private Image board = new Image("file:resources/background.jpg");
     private Image blackPawn = new Image("file:resources/blackPawn.png");
-    private FlowPane cards = new FlowPane(Orientation.HORIZONTAL);
+    private Image whitePawn = new Image("file:resources/whitePawn.png");
 
     public static void main(String[] args) {
         launch(args);
@@ -26,24 +29,28 @@ public class CheckersApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BackgroundSize backgroundSize = new BackgroundSize(500, 500, false, false, true, false);
-        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundSize size = new BackgroundSize(1100, 1100, false, false, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(board, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size);
         Background background = new Background(backgroundImage);
 
+
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
-        grid.setHgap(5.5);
-        grid.setVgap(5.5);
-        grid.setBackground(background);
 
-        ImageView img = new ImageView(blackPawn);
-        cards.getChildren().add(img);
+        for(int n=0; n<8; n++ ){
+            for (int m = 0; m<8; m++) {
+                ToggleButton button = new  ToggleButton();
+                grid.add(button, 0 + n, 0 + m);
+                if((n+m)%2 ==0 & (m<2) ){
+                    button.setGraphic(new ImageView(blackPawn));
+                } if ((n+m)%2 ==0 & (m>5) ){
+                    button.setGraphic(new ImageView(whitePawn));
+                }
+            }
+         }
 
-        grid.add(cards, 0, 0, 3, 1);
+        Group root = new Group(grid);
 
-        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
-
+        Scene scene = new Scene(root, 1000,1000);
         primaryStage.setTitle("Checkers");
         primaryStage.setScene(scene);
         primaryStage.show();
